70582049203f147b6798a0a910bffd6f3400c4d6
Joe Chea <s.chea@aupp.edu.kh>
2023-10-30 13:12:29 +0700

    initial

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer
+      className="footer footer-center p-4 bg-base-300 text-base-content"
+      style={{
+        position: "fixed",
+        left: 0,
+        bottom: 0,
+        width: "100%",
+      }}
+    >
+      <aside>
+        <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+      </aside>
+    </footer>
+  );
+}
