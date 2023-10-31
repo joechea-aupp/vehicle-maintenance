07b0bd8f343f9c1856f48b81ce8bdd63422bf139
Joe Chea <s.chea@aupp.edu.kh>
2023-10-31 10:33:39 +0700

    add responsive sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,46 @@
+export default function Sidebar() {
+  return (
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        {/* Page content here */}
+        <label
+          htmlFor="my-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay btn btn-ghost btn-circle"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h7"
+            />
+          </svg>
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          {/* Sidebar content here */}
+          <li>
+            <a>Dashboard</a>
+          </li>
+          <li>
+            <a>Insert New Maintenance Record</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
