6f8fe3a2b77030c7513f5358e68fc4a1cb0889ba
Joe Chea <s.chea@aupp.edu.kh>
2023-11-18 23:21:34 +0700

    add check id track change on item on report table

--- src/pages/maintenances/VReportItem.tsx
+++ src/pages/maintenances/VReportItem.tsx
@@ -6,6 +6,18 @@ type Props = {
 };
 
 export default function VReportItem({ reports }: Props) {
+  let reportIds: number[] = [];
+  const onCheckboxChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    reportId: number
+  ) => {
+    if (e.target.checked) {
+      reportIds.push(reportId);
+    } else {
+      reportIds = reportIds.filter((id) => id !== reportId);
+    }
+    console.log(reportIds);
+  };
   return (
     <Table.Body className="divide-y">
       {reports.map((report) => (
@@ -14,7 +26,7 @@ export default function VReportItem({ reports }: Props) {
           className="bg-white dark:border-gray-700 dark:bg-[#1d232a]"
         >
           <Table.Cell className="p-4">
-            <Checkbox />
+            <Checkbox onChange={(e) => onCheckboxChange(e, report.id)} />
           </Table.Cell>
           <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-[#9ca3af]">
             {report.vehicle}
