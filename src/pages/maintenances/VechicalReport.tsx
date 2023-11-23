53c43a7d53841961ecd29b953a33d7e43544b774
Joe Chea <s.chea@aupp.edu.kh>
2023-11-23 10:57:01 +0700

    disable anchor validation for a tag in vechicle page

--- src/pages/maintenances/VechicalReport.tsx
+++ src/pages/maintenances/VechicalReport.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { Flowbite, Label, TextInput, Checkbox, Table } from "flowbite-react";
 import { customTextInputTheme } from "../../types/CustomTheme";
 import Pagination from "../../components/Pagination";
