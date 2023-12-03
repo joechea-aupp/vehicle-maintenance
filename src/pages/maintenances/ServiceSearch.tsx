aefca6452f1e38c030d8f3ccec353f3161e924ef
Joe Chea <s.chea@aupp.edu.kh>
2023-12-03 20:49:30 +0700

    remove button on service search

--- src/pages/maintenances/ServiceSearch.tsx
+++ src/pages/maintenances/ServiceSearch.tsx
@@ -101,20 +101,6 @@ export default function ServiceSearch({
           </svg>
         </div>
 
-        <button
-          className="btn btn-square btn-outline absolute inset-y-0 right-0 btn-sm my-auto mr-1"
-          disabled={status === "pending"}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 448 512"
-            fill="#384151"
-          >
-            <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
-          </svg>
-        </button>
-
         <input
           type="text"
           placeholder="Type here"
