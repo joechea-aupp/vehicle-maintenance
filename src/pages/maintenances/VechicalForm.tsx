9aeb141a3be76ad3574375fddd6f432bd8895906
Joe Chea <s.chea@aupp.edu.kh>
2023-12-08 14:48:22 +0700

    update template placeholder item

--- src/pages/maintenances/VechicalForm.tsx
+++ src/pages/maintenances/VechicalForm.tsx
@@ -251,11 +251,9 @@ export default function VechicalForm() {
                 <option disabled selected>
                   Pick one
                 </option>
-                <option>Star Wars</option>
-                <option>Harry Potter</option>
-                <option>Lord of the Rings</option>
-                <option>Planet of the Apes</option>
-                <option>Star Trek</option>
+                <option>Month maintenance</option>
+                <option>3rd month maintance</option>
+                <option>Change tires</option>
               </select>
             </div>
           </div>
